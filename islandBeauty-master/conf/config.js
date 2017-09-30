478eaa9b21fbfa19c89fcc4c6b974d628b0724e0
MisterZhouZhou <m15038960016@163.com>
2017-09-30 16:42:46 +0800

    update

---
+++ islandBeauty-master/conf/config.js
@@ -0,0 +1,6 @@
+var conf = {
+  downloadSrc: ["http://www.torrentbit.net/cat/Adult/?list=all","http://search.hdwan.net","http://www.bttiantang.cc"],
+  downloadDir: ["../dst/av","../dst/movie"]
+};
+
+module.exports = conf;
